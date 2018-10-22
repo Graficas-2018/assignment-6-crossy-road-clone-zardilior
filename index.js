6372e839df14095a051b293526358092e5d8539c
Jose Estremadoyro <jeestremadoyro@gmail.com>
2018-10-22 09:13:41 -0500

    Added creation objects

---
+++ index.js
@@ -0,0 +1,9 @@
+// We create our app with scene, camera, world and lights
+// We create a player and add controls to it
+// The controls add to the score
+// A die function and a reset function, dies also resets the score
+// We add collisions with vehicules as a way to die
+// We add collision with trees as stopping any keyframe activity
+// We add keyframe activity to our character on move
+// We register and replicate the sections
+// We create the win reset condition when the score % number of steps forward
--- object.js
+++ object.js
@@ -7,24 +7,62 @@
  */
 var objects ={
     sectionWidth:6,
+    sectionHeight:4,
     tileDim:50,
     objects:[],
     collisions:[],
     // Character is made of  three boxes, jeans, a shirt and a head
-    object:function(color){
-        this.model = createCube(color);
-        this.intersects = function(box){
-            return this.model.intersectsBox(box);
+    object:function(color,type){
+        var obj = {};
+        obj.model = createCube(color);
+        obj.type = type;
+        obj.intersects = function(box){
+            return obj.model.intersectsBox(box);
         }
-        this.setPos = function(x,y){
-           this.model.position.x = x;  
-           this.model.position.y = y;  
+        obj.setPos = function(x,y){
+           obj.model.position.x = x;  
+           obj.model.position.y = y;  
+        }
+        if(!objects.objects['type'])
+            objects.objects['type']=[];
+        objects.objects['type'].push(obj);
+        return obj;
+    },
+    floorObject:function(color,type){
+        obj = object(color,type);
+        obj.model.position.z = -objects.tileDim;
+        return obj;
+    },
+    generateFloorObject(color,type){
+        return function(x,y){
+            var obj =  objects.floorObject(color,type);
+            obj.setPos(x,y);
+            return obj;
         }
     },
+    generateObject(color,type){
+        return function(x,y){
+            var obj =  objects.object(color,type);
+            obj.setPos(x,y);
+            return obj;
+        }
+    }
     // Sections, 
     /** A section has many objects within it and one on each tile, a tile can be created and it will      *  return a constructor function to replicate that tile */
-    section:function(){
-
+    section:function(objectMap,floorMap){
+        return function(scene){
+            var group = new THREE.Group(); 
+            for(var i in floorMap){
+                var x = i % objects.sectionWidth;
+                var y = Math.floor(i/objects.sectionWidth);
+                if(objectMap[i]!=""){
+                   group.add(objects[objectMap[i]](x,y)); 
+                }
+                group.add(objects[floorMap[i]](x,y));  
+            }
+            scene.add(group);
+            return group;
+        }
     },
     createCube: function(color){
         var material = new THREE.MeshStandardMaterial({ color: 0xff0000 });
@@ -50,3 +88,13 @@ var objects ={
         objects.collisions.push(func);
     }
 }
+// We declare the objects constructors
+objects.character = generateObject(0xf1c27d,'character');
+objects.tree = generateObject(0x42f471,'tree');
+objects.vehicule = generateObject(0x42f471,'vehicule');
+
+// We declare the floor objects constructors
+objects.water = generateFloorObject(0x41e2f4,'water');
+objects.road = generateFloorObject(0xf7f9f9,'road');
+objects.grass = generateFloorObject(0x42f471,'grass');
+objects.logs = generateFloorObject(0xa36f3c,'logs');
